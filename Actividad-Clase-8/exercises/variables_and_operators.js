6787f36268351d7e9a07c14be7ebd6e576e2184f
nicojua <138338825+nicojua@users.noreply.github.com>
2025-05-26 21:24:44 -0300

    Ejericicio sobre variables y operadores

---
+++ Actividad-Clase-8/exercises/variables_and_operators.js
@@ -0,0 +1,26 @@
+/*Pregunta 1
+Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+let num1 = 5;
+let num2 = 7;
+let suma = num1 + num2;
+
+console.log("La suma es: " + suma);
+
+/* Pregunta 2
+Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+let texto1 = "Hola, ";
+let texto2 = "mundo!";
+let textoCompleto = texto1 + texto2;
+
+console.log("Texto concatenado: " + textoCompleto);
+
+/*Pregunta 3
+Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length). */ 
+
+let palabra1 = "JavaScript";
+let palabra2 = "Lenguaje";
+let longitudTotal = palabra1.length + palabra2.length;
+
+console.log("La suma de las longitudes es: " + longitudTotal);
